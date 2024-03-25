6528ccbaa76a2fc91d7df4bdf54eaf99439867ef
Miha <Liverpoolfc291892@gmail.com>
2024-03-25 14:36:26 +0200

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.my_test_messenger"
+    compileSdk = 34
+    viewBinding.isEnabled = true
+
+    defaultConfig {
+        applicationId = "com.example.my_test_messenger"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildToolsVersion = "34.0.0"
+}
+
+dependencies {
+    implementation("com.mikepenz:materialdrawer:9.0.2")
+
+    //required support lib modules
+    implementation ("androidx.recyclerview:recyclerview:1.3.2")
+    implementation ("androidx.annotation:annotation:1.7.1")
+    implementation ("com.google.android.material:material:1.11.0") // requires at least 1.5.0-x
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Add for NavController support
+    implementation ("com.mikepenz:materialdrawer-nav:9.0.2")
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
