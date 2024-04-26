eac5de5198530c1c51ad904aac00b1c36ef1ae6c
Miha <Liverpoolfc291892@gmail.com>
2024-04-26 10:37:52 +0300

    options menu - done

--- app/src/main/java/com/example/my_test_messenger/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/my_test_messenger/ui/fragments/SettingsFragment.kt
@@ -1,11 +1,22 @@
 package com.example.my_test_messenger.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.example.my_test_messenger.R
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
     }
 
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        /**
+         * ? - Null безопасный вызов. идет обращение к активити которого нету, можно указать !! что означает точное отсутствие Null
+         * но если нет уверености лучше ставить - ?
+         */
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
+    }
 }
